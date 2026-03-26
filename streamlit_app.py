6ff568c38db8171a112fe5864ca2cb4d30bb5fab
stgitacc <150023290+stgitacc@users.noreply.github.com>
2026-03-26 23:35:22 +0000

    Update streamlit_app.py
    Rearrange API output location

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
                     values ('""" + ingredients_string + """' , '""" + name_on_order + """')"""
@@ -36,5 +38,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
