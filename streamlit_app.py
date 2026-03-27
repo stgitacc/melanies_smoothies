1a531b9d60eed4c5f7da10a940041cb2c3a8b235
stgitacc <150023290+stgitacc@users.noreply.github.com>
2026-03-27 00:08:04 +0000

    Update streamlit_app.py
    List fruit option

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME').col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -29,6 +31,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen) 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.stop()
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
                     values ('""" + ingredients_string + """' , '""" + name_on_order + """')"""
